e0ae3791658c6e3b74804c891a2515a9d27eb1f7
dmardev172 <dmardev172@gmail.com>
2026-06-04 21:24:23 +0200

    Adding com.squareup.moshi:moshi-kotlin-codegen dependency

--- app/src/main/java/pl/revolshen/simpleapp/MainActivity.kt
+++ app/src/main/java/pl/revolshen/simpleapp/MainActivity.kt
@@ -4,25 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import pl.revolshen.simpleapp.ui.theme.SimpleAppTheme
 
 class MainActivity : ComponentActivity() {
 
